a5fc9e835065cac82e344da17a4d9a73daecb625
Feagly <paolowiiu@gmail.com>
2026-05-16 11:19:52 +0200

    Slides 2

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+templates = Jinja2Templates(directory = "templates")
+app.mount("/static", StaticFiles(directory = "static"), name = "static")
 
-@app.get("/")
-def hello_world():
-    return "Hello World"
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(request=request, name="home.html", context={"text": "welcome to the store"})
+
+@app.get("/products", response_class=HTMLResponse)
+def products(request: Request):
+    product_list = [{"name": "Tastiera Meccanica", "price": 8.99, "location": "Corsia A"}]
+    return templates.TemplateResponse(request=request, name="products.html", context={"product_list": product_list})
